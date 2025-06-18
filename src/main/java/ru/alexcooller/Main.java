d552bff0520c84e04dd22a2df56b0a488b03f156
AlexCooller <Al.kulakov@bk.ru>
2025-06-19 02:00:56 +0300

    Update Main.java

--- src/main/java/ru/alexcooller/Main.java
+++ src/main/java/ru/alexcooller/Main.java
@@ -3,7 +3,7 @@ package ru.alexcooller;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    private static String msg = ""Hello qa.guru!!!"";
+    private static String msg = "Hello qa.guru!!!";
     public static void main(String[] args) {
 
         
